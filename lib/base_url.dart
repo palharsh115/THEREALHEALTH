97d29333331998e185d2a150c92f3e22433cfd4c
Palharsh115 <palharsh115@gmail.com>
2025-07-13 14:24:53 +0530

    update url

--- lib/base_url.dart
+++ lib/base_url.dart
@@ -1,3 +1,3 @@
-//String baseUrl = "https://therealhealth-backend.onrender.com";
-String baseUrl = "http://10.0.2.2:3000"; // For local development
+String baseUrl = "https://therealhealth-backend.onrender.com";
+//String baseUrl = "http://10.0.2.2:3000"; // For local development
 //String baseUrl = "https://therealhealth-backend.onrender.com";
